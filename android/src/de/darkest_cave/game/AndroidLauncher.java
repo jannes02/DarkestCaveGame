cd67bf351f1e7577650b7e041aa9cab037364717
jannes.cj@gmail.com <Call4865550100>
2021-02-16 15:18:42 +0100

    Initial commit

---
+++ android/src/de/darkest_cave/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.darkest_cave.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.darkest_cave.game.DarkestCave;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DarkestCave(), config);
+	}
+}
---
+++ ios/src/de/darkest_cave/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package de.darkest_cave.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import de.darkest_cave.game.DarkestCave;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DarkestCave(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
